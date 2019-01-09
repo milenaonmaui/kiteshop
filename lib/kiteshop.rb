fe3b836f3099acdd8c877abbcd6e50035c18594e
Milena Kari <milenakari@yahoo.com>
2019-01-09 19:26:35 +0000

    set up basic environment

--- lib/kiteshop.rb
+++ lib/kiteshop.rb
@@ -1,6 +1,9 @@
-require "kiteshop/version"
+require_relative "../lib/kiteshop/version"
+require 'nokogiri'
+require 'pry'
+require_relative "../lib/kiteshop/cli.rb"
+#module Kiteshop
+#  class Error < StandardError; end
 
-module Kiteshop
-  class Error < StandardError; end
-  # Your code goes here...
-end
+#  # Your code goes here...
+#end
---
+++ lib/kiteshop/cli.rb
@@ -0,0 +1,5 @@
+class Kiteshop::CLI 
+  def start 
+    puts "Hello World!"
+  end
+end
