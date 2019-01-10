7918a7eda9b53bc60f1871ebfac86c0a0b79a67c
Milena Kari <milenakari@yahoo.com>
2019-01-10 02:55:17 +0000

    setting up environment

--- lib/kiteshop.rb
+++ lib/kiteshop.rb
@@ -2,6 +2,8 @@ require_relative "../lib/kiteshop/version"
 require 'nokogiri'
 require 'pry'
 require_relative "../lib/kiteshop/cli.rb"
+require_relative "../lib/kiteshop/Scraper.rb"
+Bundler.require
 #module Kiteshop
 #  class Error < StandardError; end
 
--- lib/kiteshop/Scraper.rb
+++ lib/kiteshop/Scraper.rb
@@ -1,34 +1,36 @@
-class Scraper 
+class Scraper
  # url = 'https://www.realwatersports.com/collections/kites/'
 
   #index_url='https://www.realwatersports.com/collections/kites#/size_9m'
   def self.scrape_page(index_url)
     page = Nokogiri::HTML(open(index_url))
     #students = []
-    (".productgrid--item-kite .productitem--title a").text.strip
-    page.css("div.roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-        student_profile_link = "#{student.attr('href')}"
-        student_location = student.css('.student-location').text
-        student_name = student.css('.student-name').text
-        students << {name: student_name, location: student_location, profile_url: student_profile_link}
-      end
+    page.css(".productgrid--item-kite .productitem--title a").text.strip
+    arr = index_page.css(".productgrid--item-kite .productitem--title a").text.split("\n        \n")
+    products = arr.collect {|string| string.strip}
+    #page.css("div.roster-cards-container").each do |card|
+      #card.css(".student-card a").each do |student|
+      #  student_profile_link = "#{student.attr('href')}"
+      #  student_location = student.css('.student-location').text
+    #    student_name = student.css('.student-name').text
+  #      students << {name: student_name, location: student_location, profile_url: student_profile_link}
+  #    end
     end
-    students
+  #  students
   end
-  
+
 
 
 #index_page = Nokogiri::HTML(open(index_url))
 
-index_page.css(".productgrid--item-kite .productitem--title a").text.strip
-arr = index_page.css(".productgrid--item-kite .productitem--title a").text.split("\n        \n")
+#index_page.css(".productgrid--item-kite .productitem--title a").text.strip
+#arr = index_page.css(".productgrid--item-kite .productitem--title a").text.split("\n        \n")
 
-products = arr.collect {|string| string.strip}
+#products = arr.collect {|string| string.strip}
 
 ##site = 'https://www.realwatersports.com/collections/kites/products/2019-slingshot-rpm-kite'
 
 #page = Nokogiri::HTML(open(index_url))
 
 #page.css("h5 ul li")
-end
+#end
